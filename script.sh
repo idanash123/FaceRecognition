f8d0af1ad0713d11b846227671f1ecd0fb8e328b
idanash123 <idanash123@gmail.com>
2016-11-19 19:52:58 +0200

    idanash add

---
+++ script.sh
@@ -0,0 +1,7 @@
+git init
+git config user.name "idanash123"
+git config user.email "idanash123@gmail.com"
+git add *
+git commit -m "idanash add"
+git remote add origin https://github.com/idanash123/FaceRecognition
+git push origin master
